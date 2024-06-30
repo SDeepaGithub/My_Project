e6f9942ae9202837205de0a17eb5e42f31fe0aac
SDeepaGithub <Sdeepalakshmi23@gmail.com>
2024-06-30 15:57:44 +0530

    Ecommerce web

---
+++ E-Commerce_Website/script.js
@@ -0,0 +1,10 @@
+
+var sideNavbar = document.querySelector(".side-navbar-links");
+
+function showNavContent(){
+    sideNavbar.style.left = "0";
+}
+
+function closeNavContent(){
+    sideNavbar.style.left ="-70%";
+}
